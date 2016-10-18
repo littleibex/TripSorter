b098c5a47e7ee63c758460d1db3df55bc44339fc
Juzer Shabbir Husain <juzer@Juzers-MacBook-Pro.local>
2016-10-18 21:14:06 +0400

    Removed breaks after returns

--- src/PropertyFinder/BoardingCards/BoardingCardFactory.php
+++ src/PropertyFinder/BoardingCards/BoardingCardFactory.php
@@ -17,15 +17,12 @@ class BoardingCardFactory
         switch ($data[0]) {
             case 'airport_bus':
                 return new AirportBusBoardingCard($data);
-                break;
 
             case 'train':
                 return new TrainBoardingCard($data);
-                break;
 
             case 'flight':
                 return new FlightBoardingCard($data);
-                break;
 
             default:
                 throw new InvalidArgumentException('Invalid data. The mode of transportation must be set to "train", "flight" or "airport_bus".');
