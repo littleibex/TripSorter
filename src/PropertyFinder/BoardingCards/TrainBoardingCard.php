2dedeee5a184cd9286c23c0f26c88e890d4cdcd0
Juzer Shabbir Husain <juzer@Juzers-MacBook-Pro.local>
2016-10-19 20:29:12 +0400

    PhpDoc for TrainBoardingCard

--- src/PropertyFinder/BoardingCards/TrainBoardingCard.php
+++ src/PropertyFinder/BoardingCards/TrainBoardingCard.php
@@ -3,12 +3,22 @@
 namespace PropertyFinder\BoardingCards;
 
 
+use InvalidArgumentException;
+
 class TrainBoardingCard extends BoardingCard
 {
 
     protected $trainNo;
     protected $seatNo;
 
+    /**
+     * TrainBoardingCard constructor.
+     *
+     * Creates a Boarding Card for a train.
+     *
+     * @param array $data [3] => Train number.
+     *                    [4] => Seat number on the train (optional).
+     */
     public function __construct(array $data)
     {
         parent::__construct($data);
@@ -17,31 +27,63 @@ class TrainBoardingCard extends BoardingCard
         $this->_setSeatNumber($data);
     }
 
+    /**
+     * @return string Returns the train number of this card.
+     */
     public function getTrainNumber()
     {
         return $this->trainNo;
     }
 
+    /**
+     * Sets the train number of this leg received
+     * from the data in the constructor.
+     *
+     * @param array $data
+     *
+     * @throws InvalidArgumentException If the train number is empty.
+     */
     protected function _setTrainNumber(array $data)
     {
-        $this->trainNo = isset($data[3]) ? $data[3] : '';
+        if (isset($data[3]) && '' !== (string)$data[3]) {
+            $this->trainNo = $data[3];
+        } else {
+            throw new InvalidArgumentException('Invalid data. Missing required field for train: Train number.');
+        }
     }
 
+    /**
+     * @return string Returns the seat number on this train.
+     */
     public function getSeatNumber()
     {
         return $this->seatNo;
     }
 
+    /**
+     * @return string Generates and returns a string about the seat number.
+     */
     public function getSeatNumberStatement()
     {
         return $this->getSeatNumber() ? sprintf('Sit in seat %s.', $this->getSeatNumber()) : 'No seat assignment.';
     }
 
+    /**
+     * Sets the seat number on this train from the data
+     * received in the constructor.
+     *
+     * @param array $data
+     */
     protected function _setSeatNumber(array $data)
     {
         $this->seatNo = isset($data[4]) ? $data[4] : '';
     }
 
+    /**
+     * @return string Returns a message indicating how to go from
+     *                source to destination containing all the
+     *                details of this train.
+     */
     public function getMessage()
     {
         return sprintf(
