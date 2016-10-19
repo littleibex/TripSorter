b7cdfce72df7731abeed2202aefbb379951a5865
Juzer Shabbir Husain <juzer@Juzers-MacBook-Pro.local>
2016-10-19 19:53:44 +0400

    PhpDoc for Airport bus boarding card

--- src/PropertyFinder/BoardingCards/AirportBusBoardingCard.php
+++ src/PropertyFinder/BoardingCards/AirportBusBoardingCard.php
@@ -8,6 +8,13 @@ class AirportBusBoardingCard extends BoardingCard
 
     protected $seatNo;
 
+    /**
+     * AirportBusBoardingCard constructor.
+     *
+     * Creates a Boarding Card for Airport Bus.
+     *
+     * @param array $data [3] => Seat Number (optional)
+     */
     public function __construct(array $data)
     {
         parent::__construct($data);
@@ -15,21 +22,39 @@ class AirportBusBoardingCard extends BoardingCard
         $this->_setSeatNumber($data);
     }
 
+    /**
+     * @return string Returns the seat number on the bus.
+     */
     public function getSeatNumber()
     {
         return $this->seatNo;
     }
 
+    /**
+     * @return string Generates and returns a statement based on
+     *                the seat number value.
+     */
     public function getSeatNumberStatement()
     {
         return $this->getSeatNumber() ? sprintf('Sit in seat %s.', $this->getSeatNumber()) : 'No seat assignment.';
     }
 
+    /**
+     * Sets the seat number on the bus received
+     * from the data in the constructor.
+     *
+     * @param array $data
+     */
     protected function _setSeatNumber(array $data)
     {
         $this->seatNo = isset($data[3]) ? $data[3] : '';
     }
 
+    /**
+     * @return string Returns a message on how to go from
+     *                source to destination by this
+     *                Airport Bus.
+     */
     public function getMessage()
     {
         return sprintf(
